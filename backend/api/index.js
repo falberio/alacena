4f1d7367cc8d89c912be9be9bb7f5631cd8f0c70
Fran <franciscoalberio@gmail.com>
2026-01-14 23:58:08 -0300

    fix: Usar commonjs para Vercel serverless

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,4 @@
 require('dotenv/config')
 const app = require('../src/app')
 
-// Vercel Serverless Function Handler
-export default async (req, res) => {
-  return app(req, res)
-}
+module.exports = app
